8c5eecd2c98ddc7ff33c6c20782f242d41610b76
Ken Grønnbeck <kengroenn@gmail.com>
2015-05-14 13:26:29 +0200

    Run existing webhooks

--- hooks.js
+++ hooks.js
@@ -1,7 +1,66 @@
 var express = require('express');
 var mongoose = require('mongoose');
+var _ = require('lodash');
+var Q = require('q');
+var request = require('superagent');
+var Webhook = require('./lib/webhook');
 var saveHandler = require('./saveHandler')
 var app = express();
+
+function throttleExecute(task, tasks) {
+  task.execute()
+  .then(function() {
+    // should we log task completed successfully?
+  })
+  .catch(function(){
+    // we should retry tasks too
+    console.log('should log these errors or report them somehwere')
+  })
+  .finally(function() {
+    if (tasks.length == 0) return;
+    var nextTask = tasks.pop();
+    throttleExecute(nextTask);
+  });
+}
+
+function throttle(tasks, noOfConcurrentTasks) {
+  noOfConcurrentTasks = noOfConcurrentTasks || 1;
+  var startTasks = _.slice(tasks, 0, noOfConcurrentTasks + 1)
+  var restTasks = _.slice(noOfConcurrentTasks + 1, tasks.length);
+  _.forEach(startTasks, function(task) {
+    throttleExecute(task, restTasks);
+  })
+}
+
+function RequestTask (url, data) {
+  this.execute = function() {
+    var def = Q.defer();
+
+    request.post(url)
+      .send(data)
+      .set('Content-Type', 'application/json')
+      .end(function(err) {
+        if (err) return def.reject();
+        else return def.resolve();
+      });
+
+    return def.promise;
+  }
+
+  return this;
+}
+
+
+function runWebhooks (data) {
+  var MAX_NUM_OF_REQ = 10;
+  Webhook.find().exec(function(err, docs) {
+    if (err) throw new Error('DatabaseException');
+    var tasks = _.map(docs, function (doc) {
+      return new RequestTask(doc.url, data);
+    });
+    throttle(tasks, MAX_NUM_OF_REQ);
+  });
+}
 
 var Message = mongoose.model('Message', {
   channel_name: String,
@@ -41,8 +100,9 @@ app.post('/slack', function(req, res) {
       .send({success: false});
     }
 
-    return res
-    .send({ success: true });
+    res.send({ success: true });
+
+    runWebhooks(message);
   });
 });
 
